143c50631daabbc0c14b7f1a2e8778ec1b0dcb54
Dan Finelli <dan.finelli@hotmail.com>
2016-12-25 01:19:04 -0500

    login

--- library/config/routes.rb
+++ library/config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show', as: 'user'
 
 
-   get '/login', to: 'sessions#new'
-  # post '/login' => 'sessions#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   # get '/logout' => 'sessions#destroy'
 
 end
