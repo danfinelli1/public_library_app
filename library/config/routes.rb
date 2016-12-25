0ae66fd7e1bf9913345dbb2b0bf35117370f4637
Dan Finelli <dan.finelli@hotmail.com>
2016-12-25 01:01:47 -0500

    created users

---
+++ library/config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+
+  root to: 'users#index'
+
+  get '/users', to: 'users#index'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+
+
+   get '/login', to: 'sessions#new'
+  # post '/login' => 'sessions#create'
+  # get '/logout' => 'sessions#destroy'
+
+end
